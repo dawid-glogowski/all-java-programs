a39f67a476d6b9b5ec64f4425ee9a3be5f1afada
Dawid Głogowski <dg.glogowski@wp.pl>
2022-10-27 22:05:10 +0200

    1.0

---
+++ Level-0/_01-java/src/ExampleProject.java
@@ -0,0 +1,16 @@
+public class ExampleProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Zmienne -- double int float byte char boolean String -----------------------------------
+        int number = 32;
+        char variable1 = 'a';
+        String variable2 = "Hello";
+        boolean variable3 = true;
+        ExampleProject newVariable = new ExampleProject();
+
+    }
+
+
+
+}
